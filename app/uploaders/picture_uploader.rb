3df5c6b04fd07502d0e261d440e41a80c543a610
Dam Trieu Van The <dam.trieu.van.the@sun-asterisk.com>
2019-07-01 09:54:00 +0700

    Fix upload image Heroku

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,18 +1,22 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [Settings.minimagick_width,
-    Settings.minimagick_height]
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+                            Settings.minimagick_height]
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
+  #
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   def extension_whitelist
     %w(jpg jpeg gif png)
+  end
+
+  def public_id
+    return model.short_name
   end
 end
