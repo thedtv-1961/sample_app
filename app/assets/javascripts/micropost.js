40caa754200753fb70005d0d7dd20dd01e3d6fbb
Dam Trieu Van The <dam.trieu.van.the@sun-asterisk.com>
2019-07-01 17:19:26 +0700

    fix js validate, please view files change!

--- app/assets/javascripts/micropost.js
+++ app/assets/javascripts/micropost.js
@@ -1,7 +1,9 @@
-$('#micropost_picture').bind("change", function() {
-  var size_in_megabytes = this.files[0]
-    .size/Settings.size_in_megabytes/Settings.size_in_megabytes;
-  if (size_in_megabytes > 5) {
-    alert(I18n.t("img_over_size_error", {size: Settings.picture_size}));
-  }
-});
+$(document).ready(function () {
+  $('#micropost_picture').bind("change", function() {
+    var size_in_megabytes = this.files[0].size/1024/1024;
+    alert(size_in_megabytes);
+    if (size_in_megabytes < 5) {
+      alert(I18n.t("img_over_size_error", {size: Settings.picture_size}));
+    }
+  });
+})
